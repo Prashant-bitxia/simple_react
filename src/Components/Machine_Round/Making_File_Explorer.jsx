45b95aebe3d754386a3ab6b5a870f66524f2c957
Prashant-bitxia <prashant.sharma@bitxia.com>
2026-06-17 13:51:56 +0530

    making code sidebar

--- src/Components/Machine_Round/Making_File_Explorer.jsx
+++ src/Components/Machine_Round/Making_File_Explorer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import json from "../../Helper/data.json";
 
-const List = ({ list }) => {
+const List = ({ list, handleAdd, handleDelete }) => {
   const [isExpand, setIsExpand] = useState({});
 
   return (
@@ -17,7 +17,6 @@ const List = ({ list }) => {
         >
           <div
             style={{
-              border: node.isFolder ? "2px solid black" : "none",
               maxWidth: "fit-content",
               cursor: "pointer",
             }}
@@ -25,15 +24,40 @@ const List = ({ list }) => {
               setIsExpand((prev) => ({
                 ...prev,
                 [node.name]: !prev[node.name],
+                // [node.id]: !prev[node.id],
               }));
             }}
           >
             {node.isFolder && <span>{isExpand?.[node.name] ? "▼" : "▶"}</span>}
-            <span style={{}}>
-              {!node.isFolder ? ` - ` : ""} {node.name}
+            <span>
+              {!node.isFolder ? "📄" : "📁"} {node.name}
             </span>
+
+            {/*------------> Add Button <-------------*/}
+            {node.isFolder && (
+              <button
+                onClick={() => handleAdd(node.id)}
+                style={{ marginLeft: "10px", cursor: "pointer" }}
+              >
+                ➕
+              </button>
+            )}
+
+            {/*-------------> Delete Button <------------- */}
+            <button
+              onClick={() => handleDelete(node.id)}
+              style={{ marginLeft: "5px" }}
+            >
+              ❌
+            </button>
           </div>
-          {isExpand?.[node.name] && <List list={node.children} />}
+          {isExpand?.[node.name] && (
+            <List
+              list={node.children}
+              handleAdd={handleAdd}
+              handleDelete={handleDelete}
+            />
+          )}
         </div>
       ))}
     </div>
@@ -43,10 +67,70 @@ const List = ({ list }) => {
 const Making_File_Explorer = () => {
   const [data, setData] = useState(json);
 
+  // Delete Node
+  const deleteNode = (tree, nodeId) => {
+    return tree
+      .filter((node) => node.id !== nodeId)
+      .map((node) => ({
+        ...node,
+        children: node.children ? deleteNode(node.children, nodeId) : [],
+      }));
+  };
+
+  const handleAdd = (folderId) => {
+    const newNodeName = prompt("Enter a name");
+    if (!newNodeName) return;
+
+    const isFolderCreate =
+      prompt("Is Folder? (true/false)")?.toLowerCase() === "true";
+
+    const newNode = {
+      id: Date.now().toString(),
+      name: newNodeName,
+      isFolder: isFolderCreate,
+      children: isFolderCreate ? [] : undefined,
+    };
+
+    const updatedList = (list) => {
+      return list?.map((node) => {
+        if (node.id === folderId) {
+          return { ...node, children: [...node.children, newNode] };
+        }
+
+        //  if children is there then check for children kya pata vo add children mai karna cha raha ho
+        // agar parent ki id match nahi hoti to
+        if (node.children) {
+          return { ...node, children: updatedList(node.children) };
+        }
+        return node;
+      });
+    };
+
+    setData((prev) => updatedList(prev));
+  };
+
+  const handleDelete = (itemId) => {
+    const updatedList = (list) => {
+      return list
+        .filter((node) => node.id !== itemId)
+        .map((node) => {
+          if (node.children) {
+            return {
+              ...node,
+              children: updatedList(node.children),
+            };
+          }
+          return node;
+        });
+    };
+
+    setData((prev) => updatedList(prev));
+  };
+
   return (
     <div>
       <h1>Making File Exploerer</h1>
-      <List list={data} />
+      <List list={data} handleAdd={handleAdd} handleDelete={handleDelete} />
     </div>
   );
 };
